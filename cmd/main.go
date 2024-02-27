474ef0e339ec3649f0bb165c7868e3eb5cbe79e5
Gregory Komissarov <gregory.komissarov@gmail.com>
2024-02-27 10:59:44 +0300

    test

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,13 @@ import (
 	"runtime"
 )
 
-var version string = "0.0.2"
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
 
 func main() {
 	fmt.Println("hello platform:", runtime.GOOS, runtime.GOARCH)
-	fmt.Println("version:v" + version)
+	fmt.Printf("my app %s, commit %s, built at %s", version, commit, date)
 }
